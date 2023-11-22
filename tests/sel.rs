3e3a0cf375000beddb6080772194e9ac59fb1c46
linsyking <kxiang@umich.edu>
2023-11-22 13:05:47 -0500

    feat: add tests

--- tests/sel.rs
+++ tests/sel.rs
@@ -17,4 +17,18 @@ mod selection_tests {
             .move_select(CursorMove::EndOfLine);
         assert_eq!(doc.selections().len(), 1);
     }
+
+    #[test]
+    fn cursor_and_selection() {
+        let doc = Doc::from(String::from("hi, world"));
+        doc.cursors()
+            .add_cursor(3)
+            .selections()
+            .add_selection(0, 2)
+            .add_selection(0, 5)
+            .add_selection(4, 7)
+            .cursors()
+            .insert("!");
+        assert_eq!(doc.content().as_str(), "hi,! world")
+    }
 }
